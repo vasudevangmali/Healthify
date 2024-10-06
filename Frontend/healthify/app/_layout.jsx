3c1876dfb35479ac276e6fb808b9248825e45176
Riya Torgal <riyatorgal@gmail.com>
2024-10-06 18:29:00 +0530

    Add Homescreen and Fonts

--- Frontend/healthify/app/_layout.jsx
+++ Frontend/healthify/app/_layout.jsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router';
+import React, { useState, useCallback, useEffect } from 'react'
+import { View, Text } from 'react-native'
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font'
+
 
 const RootLayout = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  const [fontsLoaded, error] = useFonts({
+    "Nunito-Bold": require("../assets/fonts/Nunito-Bold.ttf"),
+    "Nunito-ExtraBold": require("../assets/fonts/Nunito-ExtraBold.ttf"),
+    "Nunito-ExtraLight": require("../assets/fonts/Nunito-ExtraLight.ttf"),
+    "Nunito-Light": require("../assets/fonts/Nunito-Light.ttf"),
+    "Nunito-Medium": require("../assets/fonts/Nunito-Medium.ttf"),
+    "Nunito-Regular": require("../assets/fonts/Nunito-Regular.ttf"),
+    "Nunito-SemiBold": require("../assets/fonts/Nunito-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if(error) throw error
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error])
+
+  if(!fontsLoaded && !error) return null;
+
+
   return (
     <Stack>
-    <Stack.Screen name='index' options={{headerShown: false}}/>
-  </Stack>
+      <Stack.Screen name='index' options={{headerShown: false}}/>
+    </Stack>
   )
 }
 
 export default RootLayout
-
