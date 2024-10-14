31155a05d4f5ced372020f92110b3c9d07348096
vasudevangmali <141993406+vasudevangmali@users.noreply.github.com>
2024-10-14 21:58:31 +0530

    Init Firebae

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
-from auth_icd import authenticate
-
-DATABASE_URL = "postgresql://postgres:root@localhost/diagnosis_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-metadata = MetaData()
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup_event():
-    access_token = authenticate()
-    
-    if access_token:
-        app.state.access_token = access_token
-    else:
-        print("Authentication failed at startup.")
-
 @app.get("/health")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "healthy"}
